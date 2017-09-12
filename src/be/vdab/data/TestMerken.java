742d2a7d95f1e17715adb455679ac8521e5c7709
Radek <radoslaw.lizak@gmail.com>
2017-09-12 15:31:28 +0200

    TestMerken klaar

---
+++ src/be/vdab/data/TestMerken.java
@@ -0,0 +1,14 @@
+package be.vdab.data;
+
+public class TestMerken {
+    public static void main(String[] args) {
+        Merken merken = new Merken();
+        System.out.println(merken);
+        merken.sorteer();
+        System.out.println(merken);
+        merken.keerOm();
+        System.out.println(merken);
+    }
+}
+
+}
