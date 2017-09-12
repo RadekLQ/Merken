dc386755d94b446b94c59320f81bb53d243664ee
Marco <m.kohnen@telenet.be>
2017-09-12 14:54:27 +0200

    added package

--- src/be/vdab/data/Data.java
+++ src/be/vdab/data/Data.java
@@ -1,3 +1,5 @@
+package be.vdab.data;
+
 public class Data {
     public static String [] merken = {"BMW", "Audi", "VW", "Ford", "Opel",
             "Renault", "Peugeot", "Citroën", "Mercedes", "Fiat"};
